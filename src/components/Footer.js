74286a7c740f0f4962c003d6b0e2c86904217854
hugoalves <81472775+hugoalvessiq@users.noreply.github.com>
2022-06-10 21:10:14 -0300

    Footer link addition

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,16 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://www.google.com">
+      <a href="https://www.twitter.com" target={"_blank"} rel='noreferrer'>
         <i className="bi bi-twitter"></i>
       </a>
-      <a href="https://www.google.com">
+      <a href="https://www.facebook.com" target={"_blank"}  rel='noreferrer'>
         <i className="bi bi-facebook"></i>
       </a>
-      <a href="https://www.google.com">
+      <a href="https://www.instagram.com" target={"_blank"}  rel='noreferrer'>
         <i className="bi bi-instagram"></i>
       </a>
-      <a href="https://www.google.com">
+      <a href="https://www.github.com" target={"_blank"}  rel='noreferrer'>
         <i className="bi bi-github"></i>
       </a>
     </footer>
